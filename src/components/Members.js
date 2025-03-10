f7c9b02d1d2eade2d4055daf04bdbeee8dceb8c2
444CChris <christopher.chen444@gmail.com>
2025-03-09 17:37:10 -0700

    Fixed members styling and added club pictures to members and home page.

--- src/components/Members.js
+++ src/components/Members.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import "../css/Members.css";
+import Lucas from "../assets/headshots/lucas.png";
+import Akshai from "../assets/headshots/akshai.png";
+import Sumedh from "../assets/headshots/sumedh.png";
+import Chris from "../assets/headshots/chris.png";
+import Evan from "../assets/headshots/evan.png";
+import Nico from "../assets/headshots/nico.png";
 
 const Members = () => {
     return (
@@ -7,48 +13,36 @@ const Members = () => {
             <div className="levelOrganization">
                 <h1>Executive Team</h1>
             </div>
-            <div className="members-row">
-                <div className="member-item">
-                    <a href="https://www.linkedin.com/in/akshaisrin/">
-                        <img src="https://blog.cdn.own3d.tv/klSI1CBgQyWinwR0XvJZ" id="bigImage" />
-                    </a>
-                    <p>Akshai Srinivasan</p>
-                </div>
-                <div className="member-item">
-                    <a href="https://www.linkedin.com/in/sumedh910/">
-                        <img src="https://blog.cdn.own3d.tv/klSI1CBgQyWinwR0XvJZ" id="bigImage" />
-                    </a>
-                    <p>Sumedh Chinta</p>
-                </div>
-                <div className="member-item">
-                    <a href="https://www.linkedin.com/in/christopher-chen-56b889342/">
-                        <img src="https://blog.cdn.own3d.tv/klSI1CBgQyWinwR0XvJZ" id="bigImage" />
-                    </a>
-                    <p>Chris Chen</p>
-                </div>
-                <div className="member-item">
-                    <a href="https://www.linkedin.com/in/lucas-varotto-353bb729a/">
-                        <img src="https://blog.cdn.own3d.tv/klSI1CBgQyWinwR0XvJZ" id="bigImage" />
-                    </a>
-                    <p>Lucas Varotto</p>
-                </div>
-            </div>
-            <div className="members-row">
-                <div className="member-item">
-                    <a href="https://www.linkedin.com/in/evan-myers-011227298/">
-                        <img src="https://blog.cdn.own3d.tv/klSI1CBgQyWinwR0XvJZ" id="bigImage" />
-                    </a>
-                    <p>Evan Myers</p>
-                </div>
-                <div className="member-item">
-                    <a href="https://www.linkedin.com/in/nicosusanto/">
-                        <img src="https://blog.cdn.own3d.tv/klSI1CBgQyWinwR0XvJZ" id="bigImage" />
-                    </a>
-                    <p>Nico Susanto</p>
-                </div>
-            </div>
+            <table>
+                <tr>
+                    <td><a href="https://www.linkedin.com/in/akshaisrin/"><img src={Akshai} id="bigImage" alt="Akshai Srinivasan"/></a></td>
+                    <td><a href="https://www.linkedin.com/in/sumedh910/"><img src={Sumedh} id="bigImage" alt="Sumedh Chinta"/></a></td>
+                    <td><a href="https://www.linkedin.com/in/christopher-chen-56b889342/"><img src={Chris} id="bigImage" alt="Christopher Chen"/></a></td>
+                    <td><a href="https://www.linkedin.com/in/lucas-varotto-353bb729a/"><img src={Lucas} id="bigImage" alt="Lucas Varotto"/></a></td>
+                </tr>
+                <tr>
+                    <td><p>Akshai Srinivasan</p></td>
+                    <td><p>Sumedh Chinta</p></td>
+                    <td><p>Christopher Chen</p></td>
+                    <td><p>Lucas Varotto</p></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td><a href="https://www.linkedin.com/in/evan-myers-011227298/"><img src={Evan} id="bigImage" alt="Evan Myers" /></a></td>
+                    <td><a href="https://www.linkedin.com/in/nicosusanto/"><img src={Nico} id="bigImage" alt="Nico Susanto"/></a></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td><p>Evan Myers</p></td>
+                    <td><p>Nico Susanto</p></td>
+                    <td></td>
+                </tr>
+            </table>
         </div>
     );
 }
 
 export default Members;
+
+
